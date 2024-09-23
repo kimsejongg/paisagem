b56fb5a9fbab5347ba1db3da9ee4fb544c96212b
kimsejongg <bruna.bueno14@escola.pr.gov.br>
2024-09-23 10:08:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  noStroke(rect)
+  fill(135, 206, 235)
+  rect(0, 0, 400, 200);
+  fill("yellow")
+  circle(140, 150, 100);
+  fill(0, 105, 148)
+  rect(0, 200, 400, 200)
+}
